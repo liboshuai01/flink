bd9e1a6d37eb36601726ab3db98f8b1a4a2e16bf
BoShuai Li <liboshuai01@gmail.com>
2024-11-19 16:49:08 +0800

    update flink-learning-rpc-socket

--- flink-learning/flink-learning-rpc/flink-learning-rpc-socket/flink-learning-rpc-socket-consumer/src/main/java/cn/liboshuai/flink/ConsumerSystem.java
+++ flink-learning/flink-learning-rpc/flink-learning-rpc-socket/flink-learning-rpc-socket-consumer/src/main/java/cn/liboshuai/flink/ConsumerSystem.java
@@ -10,38 +10,25 @@ import java.net.Socket;
 
 @Slf4j
 public class ConsumerSystem {
-    public static void main(String[] args) {
-        Socket socket = null;
-        ObjectOutputStream objectOutputStream = null;
-        ObjectInputStream objectInputStream = null;
+    public static void main(String[] args) throws Exception {
+        // 创建Socket连接到服务端
+        Socket socket = new Socket("127.0.0.1", 10001);
+        log.info("已连接到服务端: {}", socket.getRemoteSocketAddress());
 
-        try {
-            // 创建Socket连接到服务端
-            socket = new Socket("127.0.0.1", 10001);
-            log.info("已连接到服务端: {}", socket.getRemoteSocketAddress());
+        // 创建输出流，向服务端发送消息
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+        objectOutputStream.writeObject("你好啊，我是客户端-消费者");
+        objectOutputStream.flush();
 
-            // 创建输出流，向服务端发送消息
-            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-            objectOutputStream.writeObject("你好啊，我是客户端-消费者");
-            objectOutputStream.flush();
+        // 创建输入流，接收服务端的响应
+        ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+        Object object = objectInputStream.readObject();
+        // 打印服务端发送的信息
+        log.info("收到信息: {}", object);
 
-            // 创建输入流，接收服务端的响应
-            objectInputStream = new ObjectInputStream(socket.getInputStream());
-            Object object = objectInputStream.readObject();
-            // 打印服务端发送的信息
-            log.info("收到信息: {}", object);
-
-        } catch (IOException | ClassNotFoundException e) {
-            log.error("发生错误: {}", e.getMessage());
-        } finally {
-            // 关闭资源，避免内存泄漏
-            try {
-                if (objectOutputStream != null) objectOutputStream.close();
-                if (objectInputStream != null) objectInputStream.close();
-                if (socket != null) socket.close();
-            } catch (IOException e) {
-                log.error("关闭资源时发生错误: {}", e.getMessage());
-            }
-        }
+        // 关闭资源
+        objectOutputStream.close();
+        objectInputStream.close();
+        socket.close();
     }
 }
--- flink-learning/flink-learning-rpc/flink-learning-rpc-socket/flink-learning-rpc-socket-provider/src/main/java/cn/liboshuai/flink/ProviderSystem.java
+++ flink-learning/flink-learning-rpc/flink-learning-rpc-socket/flink-learning-rpc-socket-provider/src/main/java/cn/liboshuai/flink/ProviderSystem.java
@@ -3,7 +3,6 @@ package cn.liboshuai.flink;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
@@ -11,44 +10,31 @@ import java.net.Socket;
 
 @Slf4j
 public class ProviderSystem {
-    public static void main(String[] args) {
-        ServerSocket serverSocket = null;
-        Socket socket = null;
-        ObjectInputStream objectInputStream = null;
-        ObjectOutputStream objectOutputStream = null;
+    public static void main(String[] args) throws Exception {
 
-        try {
-            // 创建一个ServerSocket，监听端口10001
-            serverSocket = new ServerSocket(10001);
-            log.info("服务端已启动，等待连接...");
+        // 创建一个ServerSocket，监听端口10001
+        ServerSocket serverSocket = new ServerSocket(10001);
+        log.info("服务端已启动，等待连接...");
 
-            // 接受客户端的连接请求（同步阻塞等待客户端的连接）
-            socket = serverSocket.accept();
-            log.info("客户端已连接: {}", socket.getRemoteSocketAddress());
+        // 接受客户端的连接请求（同步阻塞等待客户端的连接）
+        Socket socket = serverSocket.accept();
+        log.info("客户端已连接: {}", socket.getRemoteSocketAddress());
 
-            // 创建输入流，用于接收客户端发送的对象
-            objectInputStream = new ObjectInputStream(socket.getInputStream());
-            Object object = objectInputStream.readObject();
-            // 打印客户端发送的信息
-            log.info("收到信息: {}", object);
+        // 创建输入流，用于接收客户端发送的对象
+        ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+        Object object = objectInputStream.readObject();
+        // 打印客户端发送的信息
+        log.info("收到信息: {}", object);
 
-            // 创建输出流，向客户端发送响应
-            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-            objectOutputStream.writeObject("你好啊，我是服务端-提供者");
-            objectOutputStream.flush();
+        // 创建输出流，向客户端发送响应
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+        objectOutputStream.writeObject("你好啊，我是服务端-提供者");
+        objectOutputStream.flush();
 
-        } catch (IOException | ClassNotFoundException e) {
-            log.error("发生错误: {}", e.getMessage());
-        } finally {
-            // 关闭资源，避免内存泄漏
-            try {
-                if (objectOutputStream != null) objectOutputStream.close();
-                if (objectInputStream != null) objectInputStream.close();
-                if (socket != null) socket.close();
-                if (serverSocket != null) serverSocket.close();
-            } catch (IOException e) {
-                log.error("关闭资源时发生错误: {}", e.getMessage());
-            }
-        }
+        // 关闭资源，避免内存泄漏
+        objectOutputStream.close();
+        objectInputStream.close();
+        socket.close();
+        serverSocket.close();
     }
 }
