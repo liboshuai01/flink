016bfcf523dc083f822bb7447fbdfb7acc42e525
Boshuai Li <liboshuai01@gmail.com>
2026-01-23 12:34:41 +0800

    refactor(flink-examples-streaming): 将自定义WordCount从batch模块迁移至streaming模块

--- flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/lbs/WordCount.java
+++ flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/lbs/WordCount.java
@@ -1,4 +1,4 @@
-package org.apache.flink.examples.java.lbs;
+package org.apache.flink.streaming.examples.lbs;
 
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
